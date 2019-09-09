bbc001ce22122ac9ff6745e6f5fd283a89a81e6e
Daylan <daylanberry@gmail.com>
2019-09-09 14:55:39 -0700

    Completed Basic req

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen'
+  this.canFly = true;
+  this.treasureChest = [];
+
 };
+
+Grub.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure)
+}
+
+
+ForagerBee.prototype = Object.create(Grub.prototype)
+ForagerBee.prototype.constructor = ForagerBee
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey'
+  this.canFly = true;
+  this.treasureChest = [];
+  this.honeyPot = 0;
+
 };
 
+Grub.prototype.makeHoney = function(){
+  this.honeyPot++
+}
+
+Grub.prototype.giveHoney = function(){
+  this.honeyPot--
+}
+
+HoneyMakerBee.prototype = Object.create(Grub.prototype)
+HoneyMakerBee.prototype.constructor = HoneyMakerBee
